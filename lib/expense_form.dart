03e4ca99bad0e1e2a722e3fe4160e0760d9d1d2e
Thomas Willingham <thomas@tshows.us>
2019-09-11 04:05:36 -0500

    feat(form): improve ui/ux

--- lib/expense_form.dart
+++ lib/expense_form.dart
@@ -8,6 +8,8 @@ import 'package:spensly/camera.dart';
 import 'package:spensly/database_helpers.dart';
 import 'package:path/path.dart' show join;
 
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+
 
 import 'package:camera/camera.dart';
 
@@ -33,7 +35,12 @@ class _ExpenseDialogState extends State<ExpenseDialog> {
   final _formKey = GlobalKey<FormState>();
   final Expense expense;
   _ExpenseDialogState({this.expense});
-  File imageFile = null;  
+  File imageFile = null; 
+  String _category = ''; 
+
+  FocusNode name = new FocusNode();
+  FocusNode amount = new FocusNode();
+  FocusNode vendor = new FocusNode();
 
   @override
   initState() {
@@ -90,121 +97,148 @@ class _ExpenseDialogState extends State<ExpenseDialog> {
         ),
       );               
     }
+    
 
     return AlertDialog(
       content: Form(
         key: _formKey,
-        child: SingleChildScrollView(
-   child:  Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text(DateFormat.yMMMMd("en_US").format(expense.date)),
-                  Expanded(
-                    child: IconButton(
-                    icon: Icon(Icons.calendar_today),
-                    onPressed: () => _selectDate(context),
-                  )),
-                ]
+        child: Container(
+          width: double.maxFinite,
+          child: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            shrinkWrap: true,
+            children: <Widget>[
+              DateTimeField(
+                initialValue: expense.date,
+                enableInteractiveSelection: false,
+                readOnly: true,
+                resetIcon: null,
+                format: DateFormat('MM/dd/yyyy'),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.calendar_today),
+                  labelText: 'Date',
+                ),
+                onChanged: (picked) {
+                  setState(() =>
+                    expense.date = picked);
+                },
+                onShowPicker: (context, currentValue) {
+                  return showDatePicker(
+                      context: context,
+                      firstDate: DateTime(1900),
+                      initialDate: expense.date ?? DateTime.now(),
+                      lastDate: DateTime(2100));
+                },
+                textInputAction: TextInputAction.next,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextFormField(
+              TextFormField(
                 initialValue: expense.name,
                 decoration: InputDecoration(
+                  icon: Icon(Icons.title),
                   labelText: 'Name',
                 ),
                 onSaved: (val) {
                   setState(() =>
                       expense.name = val);
+                },
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (term){
+                  // process
                 }
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextFormField(
-                
+              TextFormField(
                 initialValue: expense.amount.toString(),
                 decoration: InputDecoration(
+                  icon: Icon(Icons.attach_money),
                   labelText: 'Amount',
                 ),
+                keyboardType: TextInputType.number,
                 onSaved: (val) {
                   setState(() =>
                       expense.amount = double.parse(val));
-                }
+                },
+                onFieldSubmitted: (term){
+                  // process
+                },
+                textInputAction: TextInputAction.next
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextFormField(
+              TextFormField(
                 initialValue: expense.vendor,
                 decoration: InputDecoration(
+                  icon: Icon(Icons.store),
                   labelText: 'Vendor',
                 ),
                 onSaved: (val) {
                   setState(() =>
                       expense.vendor = val);
+                },
+                onFieldSubmitted: (term){
+                  // process
+                },
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next
+              ),
+              FormField(
+                builder: (FormFieldState state) {
+                  return InputDecorator(
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.category),
+                      labelText: 'Category',
+                    ),
+                    isEmpty: expense.category == '',
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        value: expense.category,
+                        items: expenseCategoryList.map((String category) {
+                          return  DropdownMenuItem<String>(
+                            value: category,
+                            child: new Text(expenseCategories[category]['name']),
+                          );
+                        }).toList(),
+                        onChanged: (val) {
+                          setState(() =>
+                            expense.category = val);
+                        },
+                      )
+                    )
+                  );
                 }
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  DropdownButton<String>(
-                    value: expense.category,
-                    items: expenseCategoryList.map((String category) {
-                      return  DropdownMenuItem<String>(
-                        value: category,
-                        child: new Text(expenseCategories[category]['name']),
-                      );
-                    }).toList(),
-                    onChanged: (val) {
-                      setState(() =>
-                        expense.category = val);
-                    },
-                  )
-                ]
-              ),
-            ),
 
-            // photo area
-            GestureDetector(
-              onTap: () => takePhoto(),
-              child: Container(
-                width: 100.0,
-                height: 150.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                  color: Colors.redAccent,
+              // photo area
+              GestureDetector(
+                onTap: () => takePhoto(),
+                child: Container(
+                  width: 100.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    color: Colors.redAccent,
+                  ),
+                  child: imageFile != null
+                    ? Image.file(imageFile)
+                    : Center(child: Icon(Icons.camera))
+                )
+              ),
+
+              // create button
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  child: expense.id == null ? Text("Create") : Text("Update"),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      widget.onSubmit(expense, widget.index);
+                      Navigator.pop(context);
+                    }
+                  },
                 ),
-                child: imageFile != null
-                  ? Image.file(imageFile)
-                  : Center(child: Icon(Icons.camera))
               )
-            ),
-
-            // create button
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                child: Text("Create"),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    _formKey.currentState.save();
-                    widget.onSubmit(expense, widget.index);
-                    Navigator.pop(context);
-                  }
-                },
-              ),
-            )
-          ],
-        ),
+            ],
+          ),
+        )
       )
-    ));
+    );
   }
 }
